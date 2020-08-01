3ba1723ff62e7d0732cf422dc7ce5f31fd5a4162
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-08-01 20:53:47 +0200

    Complete run docker script

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t mohamedsgap/perdiction:latest
 
 # Step 2: 
 # List docker images
+docker image ls 
 
 # Step 3: 
 # Run flask app
+docker run --name perdiction -p 8000:80 mohamedsgap/perdiction:latest
