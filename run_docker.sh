077b8827607834b0e7f8893e5c5c861e6f7c9d79
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-08-02 00:41:11 +0200

    Setup run_docker script

--- run_docker.sh
+++ run_docker.sh
@@ -8,8 +8,8 @@ docker build -t mohamedsgap/perdiction:latest
 
 # Step 2: 
 # List docker images
-docker image ls 
+docker image ls
 
 # Step 3: 
 # Run flask app
-docker run --name perdiction -p 8000:80 mohamedsgap/perdiction:latest
+docker run --name prediction -p 8000:80 mohamedsgap/prediction:latest
