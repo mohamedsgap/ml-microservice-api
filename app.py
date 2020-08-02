9bed7718245165698eb79d0b36a627c513c9acce
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-08-02 04:38:16 +0200

    Fix CircleCI lint issue

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
