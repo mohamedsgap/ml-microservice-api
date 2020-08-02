f5f51ecc4601232013522022a28514b6e4c556e4
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-08-02 03:08:23 +0200

    Run kube claster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="mohamedsgap/prediction"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run prediction\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=prediction-api
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward prediction 8000:80
